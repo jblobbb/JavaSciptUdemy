3b37f7f3e88a0cc1333e05cde58ee7747e8ce8ed
Johnny <johnnyhunto@googlemail.com>
2024-08-09 01:14:59 +0100

    corrected pangram

--- Functions/is-pangram.js
+++ Functions/is-pangram.js
@@ -1,21 +1,20 @@
-// function isPangram(sentence) {
-//     const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
-//     let lowercased = sentence.toLowerCase()
-//     for(i=0; i<=alphabet.length; i++) {
-//         if(lowercased.indexOf(alphabet[i]) === -1) {
-//             console.log(i);
-//             return false;
-//         }
-//     }
-//     return true;
-// }
-
 function isPangram(sentence) {
-    const alphabetSet = new Set();
-    for (let char of sentence.toLowerCase()) {
-        if (char >= 'a' && char <= 'z') {
-           alphabetSet.add(char);
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '];
+    let lowercased = sentence.toLowerCase()
+    for(i=0; i<alphabet.length; i++) {
+        if(!lowercased.includes(alphabet[i])) {
+            return false;
         }
     }
-    return alphabetSet.size === 26;
+    return true;
 }
+
+// function isPangram(sentence) {
+//     const alphabetSet = new Set();
+//     for (let char of sentence.toLowerCase()) {
+//         if (char >= 'a' && char <= 'z') {
+//            alphabetSet.add(char);
+//         }
+//     }
+//     return alphabetSet.size === 26;
+// }
